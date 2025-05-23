8aa834078afb02670e705c1826c7f6b8c19529da
Jorge Muñoz Guerrero <jorgemunoz1991@gmail.com>
2025-05-23 14:05:49 +0200

    Add PWA support

--- src/index.tsx
+++ src/index.tsx
@@ -9,3 +9,15 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('✅ ServiceWorker registered: ', registration);
+      })
+      .catch(error => {
+        console.error('❌ ServiceWorker registration failed: ', error);
+      });
+  });
+}
